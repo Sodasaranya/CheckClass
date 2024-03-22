21a4364ec800c4f8e25623ef73620a2aa0652e0d
Sodasaranya <153315065+Sodasaranya@users.noreply.github.com>
2024-03-23 04:17:23 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD7CKlzfJN96YAVi9dhoI2oiXR8-2ErmiI",
+          apiKey: "AIzaSyD7CKlzfJN96YAVi9dhoI2oiXR8-2ErmiI",
           authDomain: "check-class-cf176.firebaseapp.com",
           projectId: "check-class-cf176",
           storageBucket: "check-class-cf176.appspot.com",
@@ -16,11 +16,9 @@ const firebaseConfig = {
           appId: "1:44347073660:web:3ba2ce241b9b5e8c5f95f9",
           measurementId: "G-C9ZL0K6BZN"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore(app);
-
 export { auth, db };
 export default app;
